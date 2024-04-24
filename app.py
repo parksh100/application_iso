44eab9c3f04cdd8c207cff70e93e1cc9ac6e8499
parksh100 <parksh1000@outlook.com>
2024-04-24 09:18:52 +0900

    first

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Hello World!')
+
+# 페이지 설정
+st.set_page_config(
+    page_title="Streamlit Hello World",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    
+    )
